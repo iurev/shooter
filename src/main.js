91a0758dc4d4ef617b5e29dafc14c665b1ba8d3e
wwju <wwju@yandex.ru>
2016-03-13 12:57:57 +0700

    add bullets

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ var ReactDOM = require('react-dom');
 var keyboardJS = require('keyboardjs');
 var us = require('underscore');
 var map = require('./map.json');
+var Immutable = require('immutable');
+var Paper = require('paper');
+var Point = Paper.Point;
 
 var randomColor = function(red=29, green=156, blue=73, difference = 10) {
   red = red + us.random(-difference, difference)
@@ -10,6 +13,11 @@ var randomColor = function(red=29, green=156, blue=73, difference = 10) {
   blue = blue + us.random(-difference, difference)
   return 'rgb(' + red + ',' + green + ',' + blue + ')';
 };
+
+var HeroPosition = {
+  x: 800,
+  y: 800
+}
 
 var Bush = React.createClass({
   getInitialState: function() {
@@ -63,8 +71,8 @@ var Bush = React.createClass({
 var Hero = React.createClass({
   getInitialState: function() {
     return  {
-      x: 800,
-      y: 800,
+      x: HeroPosition.x,
+      y: HeroPosition.y,
       rectStyle: {
         cx: 0,
         cy: 0,
@@ -98,6 +106,8 @@ var Hero = React.createClass({
     var xIndex = Math.floor(x / 100);
     var yIndex = Math.floor(y / 100);
     if(map[xIndex][yIndex].cellType === 'water') { return }
+    HeroPosition.x = x;
+    HeroPosition.y = y;
     this.setState({
       x,
       y,
@@ -179,6 +189,46 @@ var Grass = React.createClass({
   }
 });
 
+var Bullet = React.createClass({
+  getInitialState: function() {
+    var translate = `translate(${this.props.tX}px, ${this.props.tY}px)`
+    var inState = {
+      key: Math.random(),
+      groupStyle: {
+        transform: `translate(${this.props.x}px, ${this.props.y}px)`,
+        transition: 'transform 1s ease-out',
+      },
+      circleStyle: {
+        x: 0,
+        y: 0,
+        fill: randomColor(211,59,77),
+        strokeWidth: 0,
+        stroke:'rgb(0,0,0)',
+        r: 10,
+      }
+    };
+    var that = this;
+
+    setTimeout(function () {
+      var newState = JSON.parse(JSON.stringify(inState));
+      newState.groupStyle.transform = translate;
+      that.setState(newState);
+      that.forceUpdate();
+    }, 1);
+    return inState;
+  },
+  render: function() {
+    // console.log('render');
+    // console.log(this.state);
+    return (
+      <g key={this.state.key} style={this.state.groupStyle}>
+        <circle style={this.state.circleStyle} />
+      </g>
+    );
+  }
+});
+
+
 var GrassField = React.createClass({
   getInitialState: function() {
     var grasses = us.range(30).map(function (_, xIndex) {
@@ -197,7 +247,8 @@ var GrassField = React.createClass({
     return {
       x: 0,
       y: 0,
-      grasses
+      grasses,
+      bullets: []
     };
   },
   move: function(direction) {
@@ -237,17 +288,45 @@ var GrassField = React.createClass({
       that.move('down');
     });
   },
+  bullet: function(e) {
+    // debugger
+    // var state = Immutable.Map(this.state);
+    // var newBullets = Immutable.List.of(...this.state.bullets);
+    var newBullets = this.state.bullets.slice();
+    var targetX = e.clientX - this.state.x;
+    var targetY = e.clientY - this.state.y;
+    var xPoint = targetX - HeroPosition.x;
+    var yPoint = targetY - HeroPosition.y;
+    var point = new Point(xPoint, yPoint);
+    // console.log(point);
+    point.length = 1000;
+    // console.log(point);
+    targetX = point.x + HeroPosition.x;
+    targetY = point.y + HeroPosition.y;
+    newBullets.push(
+      (
+        <Bullet x={HeroPosition.x} y={HeroPosition.y} tX={targetX} tY={targetY} key={Math.random()}/>
+      )
+    )
+    // console.log(newBullets);
+    // var newState = state.set('bullets', newBullets);
+    // console.log(newState.toObject());
+    this.setState({bullets: newBullets});
+    // console.log(this.state);
+  },
   render: function() {
     // console.log('render grass field');
     var positionCss = {
       transition: 'transform 0.05s linear',
       transform: 'translate(' + this.state.x + 'px,' + this.state.y + 'px)'
     }
+    // console.log(this.state);
     return (
-      <svg width="100%" height="100%" onKeyDown={this.onKeyDown} key="1">
+      <svg width="100%" height="100%" onKeyDown={this.onKeyDown} key="1" onClick={this.bullet}>
         <g style={positionCss}>
           {this.state.grasses}
           <Hero />
+          {this.state.bullets}
           <Bush x="1000" y="1900"/>
           <Bush x="1900" y="1200"/>
         </g>
